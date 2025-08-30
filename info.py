a06ae17785c26508d2f9c35cf972eb6d0e1e32ac
totaflix <starastar230@gmail.com>
2025-08-30 22:27:14 +0600

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ else:
 IS_FSUB = is_enabled('IS_FSUB', False)
 
 # support group
-SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '') #support group id ex:  -1002936246860
+SUPPORT_GROUP = environ.get('SUPPORT_GROUP', '-4811996966') #support group id ex:  -1002936246860
 if len(SUPPORT_GROUP) == 0:
     print('Error - SUPPORT_GROUP is missing, exiting now')
     exit()
