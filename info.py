fd4e161ba8cd73f14d2e97caf617f0906dea22a3
totaflix <starastar230@gmail.com>
2025-08-30 22:18:11 +0600

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ else:
 INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002947271067').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
-AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '').split()]
+AUTH_CHANNEL = [int(auth_channels) for auth_channels in environ.get('AUTH_CHANNEL', '-1002967713076').split()]
 if len(AUTH_CHANNEL) == 0:
     print('Info - AUTH_CHANNEL is empty')
 LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002947271067') #bot log channel -1005293546253
@@ -80,7 +80,7 @@ SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/Rkbotzsupport')
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Rkbotz')
 FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/RkMovie_group')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/Rkbotz")
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Rkbotz")
+VERIFY_TUTORIAL = envi-1002967713076et("VERIFY_TUTORIAL", "https://t.me/Rkbotz")
 
 # Bot settings
 DELETE_TIME = int(environ.get('DELETE_TIME', 3600)) # Add time in seconds 
